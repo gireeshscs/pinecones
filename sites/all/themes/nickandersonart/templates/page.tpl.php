0a47c2b6407f52c89012aa2b40ea2a8f34b357ac
orgeshkovskiy <alex.orgeshkovskiy@faceit-team.com>
2013-03-27 08:21:31 +0200

    add Nick updates, checkbox.js fixed, new DB

--- sites/all/themes/nickandersonart/templates/page.tpl.php
+++ sites/all/themes/nickandersonart/templates/page.tpl.php
@@ -78,68 +78,28 @@
                     <h1><a name="top" href="/"><span>Third Eye Pinecones</span></a></h1>
                 </div><!-- /#logo -->
                 <?php endif;?>
-
-            <div class="navbar topNav">
-                <div class="navbar-inner">
-                    <div class="container">
-                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </a>
-                        <h4>Navigation</h4>
-                        <?php if ($user->uid): ?>
-                        <div class="auth_user">
-                            <div class="Wel_name">
-                                Welcome, <?php print $user->name; ?>
-                                <?php endif;?>
-                            </div>
-
-                            <div class="logoUt_btn">
-                                <!--If loggen in -->
-                                <?php if ($user->uid): ?>
-                                <?php print l(t('Logout'), 'user/logout'); ?>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                        <div class="nav-collapse collapse navbar-responsive-collapse">
-                            <div id="cart_link">
-                                <?php if($page['cart_region']):?>
-                                    <?php print render($page['cart_region']);?>
-                                <?php endif;?>
-                            </div><!-- /#cart_link -->
-                            <div id="search">
-                                <?php $block = module_invoke('search', 'block_view', 'form');
-                                print render($block['content']);
-                                ?>
-                            </div><!-- /#search -->
-                            <div id="social_links" class="clearfix">
-                                <ul>
-                                    <li><a id="social_links_facebook" href="#"><span>Facebook</span></a></li>
-                                    <li><a id="social_links_twitter" href="#"><span>Twitter</span></a></li>
-                                </ul>
-                            </div><!-- /#social_links -->
-
-
-                            <div id="main_menu">
-                                <?php if($page['topmenu']):?>
-                                    <?php print render($page['topmenu']);?>
-                                <?php endif;?>
-                            </div><!-- /#main_menu -->
-
-                            <div id="sub_menu">
-                                <?php if($page['submenu']): ?>
-                                    <?php print render($page['submenu'])?>
-                                <?php endif;?>
-                            </div><!-- /#sub_menu -->
-
-                        </div><!-- /.nav-collapse -->
-                    </div>
-                </div><!-- /navbar-inner -->
-            </div>
-
-
-
+            <div id="main_menu">
+                <?php if($page['topmenu']):?>
+                    <?php print render($page['topmenu']);?>
+                <?php endif;?>
+            </div><!-- /#main_menu -->
+          <div id="sub_menu">
+              <?php if($page['submenu']): ?>
+                <?php print render($page['submenu'])?>
+            <?php endif;?> 
+          </div><!-- /#sub_menu -->
+          <div id="cart_link">
+            <?php if($page['cart_region']):?>
+                <?php print render($page['cart_region']);?>
+            <?php endif;?>  
+          </div><!-- /#cart_link -->
+          <div id="social_links">
+            <ul>
+              <li><a id="social_links_facebook" href="http://www.facebook.com/ThirdEyePinecones"><span>Facebook</span></a></li>
+              <li><a id="social_links_twitter" href="https://twitter.com/3rdEyePinecones"><span>Twitter</span></a></li>
+              <li><a id="social_links_pin" href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Ftepnew.nickandersonart.com&media=http%3A%2F%2Ftepnew.nickandersonart.com%2Fsites%2Fdefault%2Ffiles%2Fscreenshot.png" class="pin-it-button" count-layout="horizontal"></a>
+            </ul>
+          </div><!-- /#social_links -->
           <div id="email_signup">
             
             <!--If anonim user-->  
@@ -151,7 +111,16 @@
             <?php endif; ?>    
             <!--If logget in, print username-->
             
+            <?php if ($user->uid): ?>
+            <div class="auth_user">
+                Welcome, <?php print $user->name; ?>
+            <?php endif;?>
 
+            <!--If loggen in -->
+            <?php if ($user->uid): ?>
+                <?php print l(t('Logout'), 'user/logout'); ?>
+            </div> 
+            <?php endif; ?>
                    
           </div><!-- /#email_signup -->
           <?php 
@@ -161,24 +130,23 @@
           <?php if ($show_messages && $messages): ?>
               <div id="lightboxAutoModal" style="display: none;" ><?php print $messages; ?></div>
           <?php endif; ?>    
-
+          <div id="search">
+             <?php $block = module_invoke('search', 'block_view', 'form');
+                    print render($block['content']);
+             ?> 
+          </div><!-- /#search -->
         </div><!-- /#header -->
       </div><!-- /#header_row -->
-
       <?php if($page['hero']) : ?>
-       <div class="heroWrapp">
-          <div id="hero">
-            <?php print render($page['hero']); ?>
-
-          </div><!-- /#hero -->
-            <?php if($page['hero_blocks']) : ?>
-                <div id="hero_blocks" class="clearfix">
-                    <?php print render($page['hero_blocks']); ?>
-                </div><!-- /#hero_blocks -->
-            <?php endif; ?>
-       </div>
+      <div id="hero">
+        <?php print render($page['hero']); ?>
+        <?php if($page['hero_blocks']) : ?>
+        <div id="hero_blocks">
+          <?php print render($page['hero_blocks']); ?>
+        </div><!-- /#hero_blocks -->
+        <?php endif; ?>
+      </div><!-- /#hero -->
       <?php endif; ?>
-
       <?php if(!$is_front) : ?>
       <div id="content_row" class="floatcontainer">
         <?php if($page['sidebar']) : ?>
