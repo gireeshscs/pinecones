31ab62760bc6955a1cba4cb9883a988d74464887
orgeshkovskiy <alex.orgeshkovskiy@faceit-team.com>
2013-03-28 13:53:25 +0200

    Exposed filter on product page (Chrome fix)

--- sites/all/themes/nickandersonart/js/checkbox.js
+++ sites/all/themes/nickandersonart/js/checkbox.js
@@ -10,34 +10,57 @@ $(function collapsedTree(){
         $('.collapse_filter').find('label').addClass("passive");
             $('.collapse_filter').children('div').children('input').click(function(){
                 
+                $(this).parent('div').parent('.collapse_filter').children('ul.bef-tree-depth-1').slideDown();
+                
+                if(!$(this)[0].checked && $(this).parent('div').parent('.collapse_filter').children('div ul').children('li').find('input').prop('checked') === false) {
+                       
+                        $(this).parent('div').parent('.collapse_filter').children('ul.bef-tree-depth-1').slideUp();
+                        $(this).parent('div').parent('.collapse_filter').children('div').children('label').toggleClass("passive");
+                        $(this).parent('div').parent('.collapse_filter').children('div').children('label').toggleClass("active");
+                    
+                }
+                    
+                $(this)[0].checked = true;
+                
                 $(this).parent('div').parent('.collapse_filter').addClass('parent');
                 $(this).parent('div').parent('.collapse_filter').children('div ul').children('li').addClass('childrens');
            
                 $(this).parent('div').parent('.collapse_filter').children('div').children('label').toggleClass("passive");
                 $(this).parent('div').parent('.collapse_filter').children('div').children('label').toggleClass("active");
            
-           $(this).parent('div').parent('.collapse_filter').children('ul.bef-tree-depth-1').slideDown();
+                //$(this).parent('div').parent('.collapse_filter').children('ul.bef-tree-depth-1').slideDown();
            
            if($(this).prop('checked') === true && $(this).parent('div').parent('.collapse_filter').children('div ul').children('li').find('input').prop('checked') === true){
+                  
                     $(this).prop('checked', true);
                     $(this).parent('div').parent('.collapse_filter').children('div ul').children('li').find('input').prop('checked', false);
+                
                 } else {
+                    
                     if($(this).prop('checked') === false && $(this).parent('div').parent('.collapse_filter').children('div ul').children('li').find('input').prop('checked') === false){
+                        
                         $(this).prop('checked', false);
                         $(this).parent('div').parent('.collapse_filter').children('ul.bef-tree-depth-1').slideUp();
                         $(this).parent('div').parent('.collapse_filter').children('div').children('label').toggleClass("passive");
                         $(this).parent('div').parent('.collapse_filter').children('div').children('label').toggleClass("active");
+                    
                     }
+                
+                    if ($(this)[0].checked) {
+                        $(this)[0].checked = false;
+                    }
+                    
                 }
-           
+                
                 $('ul.bef-tree-depth-1').find('label').removeClass("passive");
             });
-
-            //when we click on children item - only one must be active 
-            
            
-            $('.collapse_filter').children('ul').children('li').find('input').click(function(){
-                
+        }
+});
+
+ //when we click on children item - only one must be active 
+ $('.collapse_filter').children('ul').children('li').find('input').click(function(){
+                $(this)[0].checked = false;
                    var $parent_input = $(this).parent('div').parent('li').parent('ul').parent('.collapse_filter').children('div').children('input');
                     
                    $parent_input.prop('checked', true);
@@ -49,11 +72,8 @@ $(function collapsedTree(){
                         $parent_input.prop('checked', false);
                         $(this).attr('checked');
                     }
-                     
+                 
             });    
         
-        }
-});
-
 
 })(jQuery, Drupal, this, this.document);    
